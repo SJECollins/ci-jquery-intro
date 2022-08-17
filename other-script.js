96601ef1a2718d34bb878891a77150bdb6c0c909
SCollins <jaybuckco@gmail.com>
2022-08-17 14:44:16 +0000

    this lesson

---
+++ other-script.js
@@ -0,0 +1,32 @@
+// $("p").click(function() {
+//   $("p").slideToggle("slow");
+// })
+
+// $("p").click(function() {
+//   $(this).slideToggle("slow");
+// })
+
+// $("button").mouseenter(function() {
+//   $(this).removeClass("make-red").addClass("make-blue")
+// })
+
+// $("button").mouseleave(function() {
+//   $(this).removeClass("make-blue").addClass("make-red")
+// })
+
+$(document).ready(function() {
+  $(".box").on("click", function() {
+    // let classNames = $(this).attr("class").split(" ");
+    // $("." + classNames[1]).css("background-color", "red");
+
+    let classNames = $(this).attr("class").split(" ");
+    let boxClass = classNames[0];
+    let className = classNames[1];
+    if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+      $("." + className).css("background-color", "#000");
+    } else {
+      $("." + className).css("background-color", "#000");
+      $("." + className).css("background-color", "red");
+    }
+  });
+});
--- script.js
+++ script.js
@@ -1,20 +1,101 @@
-$(document).ready(function() {
-  $("#stream1_btn").on("click", function() {
-      $(".stream1").removeClass('highlight_stream');
-      $(".stream2").removeClass('highlight_stream');
-      $(".stream3").removeClass('highlight_stream');
-      $(".stream1").addClass('highlight_stream');
- });
- $("#stream2_btn").on("click", function() {
-      $(".stream1").removeClass('highlight_stream');
-      $(".stream2").removeClass('highlight_stream');
-      $(".stream3").removeClass('highlight_stream');
-      $(".stream2").addClass('highlight_stream');
- });
- $("#stream3_btn").on("click", function() {
-      $(".stream1").removeClass('highlight_stream');
-      $(".stream2").removeClass('highlight_stream');
-      $(".stream3").removeClass('highlight_stream');
-      $(".stream3").addClass('highlight_stream');
- });
-}); 
+// $(document).ready(function() {
+     //onClick event that runs code when user clicks
+//   $("#stream1_btn").on("click", function() {
+//       $(".stream1").removeClass('highlight_stream');
+//       $(".stream2").removeClass('highlight_stream');
+//       $(".stream3").removeClass('highlight_stream');
+//       $(".stream1").addClass('highlight_stream');
+//  });
+//  $("#stream1_btn").click(function() {
+//      $(".stream1").show();
+//      $(".stream1").hide("slow");
+//      $(".stream1").toggle("medium");
+//      $(".stream1").slideUp("fast");
+//      $(".stream1").slideDown(1000);
+//  })
+
+//  $("#stream2_btn").click(function() {
+//       $(".stream1").slideToggle();
+//       $(".stream2").fadeTo();
+//       $(".stream3").fadeOut();
+//       $(".stream2").fadeToggle();
+//  });
+//  $("#stream3_btn").mouseenter(function() {
+//       $(".stream1").removeClass('highlight_stream');
+//       $(".stream2").removeClass('highlight_stream');
+//       $(".stream3").removeClass('highlight_stream');
+//       $(".stream3").addClass('highlight_stream');
+//  });
+
+ 
+// }); 
+// $(document).ready(function() {
+//      // Create the slideToggle effects each of the paragraphs and
+//      // buttons
+//      $("#button_effects1").click(function(){
+//          $('#par1').slideToggle('1000');
+//      });
+//      $("#button_effects2").click(function(){
+//          $('#par2').slideToggle('1000');
+//      });
+//      $("#button_effects3").click(function(){
+//          $('#par3').slideToggle('1000');
+//      });
+//      $("#button_effects4").click(function(){
+//          $('#par4').slideToggle('1000');
+//      });
+//      $("#button_effects5").click(function(){
+//          $('#par5').slideToggle('1000');
+//      });
+//      $("#button_effects6").click(function(){
+//          $('#par6').slideToggle('1000');
+//      });
+ 
+//      // Use the fadeTo effect when the mouse hovers over a specific button
+//      // and fadeTo again when the mouse is no longer hovering over the button
+//      $("#button_effects1").mouseenter(function(){
+//          $('#button_effects1').fadeTo(1000, 0.5);
+//      });
+//      $("#button_effects1").mouseleave(function(){
+//          $('#button_effects1').fadeTo(1000, 1);
+//      });
+ 
+//      $("#button_effects2").mouseenter(function(){
+//          $('#button_effects2').fadeTo(1000, 0.5);
+//      });
+//      $("#button_effects2").mouseleave(function(){
+//          $('#button_effects2').fadeTo(1000, 1);
+//      });
+ 
+//      $("#button_effects3").mouseenter(function(){
+//          $('#button_effects3').fadeTo(1000, 0.5);
+//      });
+//      $("#button_effects3").mouseleave(function(){
+//          $('#button_effects3').fadeTo(1000, 1);
+//      });
+ 
+//      $("#button_effects4").mouseenter(function(){
+//          $('#button_effects4').fadeTo(1000, 0.5);
+//      });
+//      $("#button_effects4").mouseleave(function(){
+//          $('#button_effects4').fadeTo(1000, 1);
+//      });
+ 
+//      $("#button_effects5").mouseenter(function(){
+//          $('#button_effects5').fadeTo(1000, 0.5);
+//      });
+//      $("#button_effects5").mouseleave(function(){
+//          $('#button_effects5').fadeTo(1000, 1);
+//      });
+ 
+//      $("#button_effects6").mouseenter(function(){
+//          $('#button_effects6').fadeTo(1000, 0.5);
+//      });
+//      $("#button_effects6").mouseleave(function(){
+//          $('#button_effects6').fadeTo(1000, 1);
+//      });
+ 
+//      $("#myButton").removeClass("blueBox").addClass("border");
+//      $("p").css("color", "blue").slideUp(2000).slideDown(2000);
+//      $("a").attr("href", "http://www.example.com");
+//  }); 
